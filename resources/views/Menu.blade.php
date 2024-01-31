40ad6fe8b2766d6a8a6f9df9878f793b337119e5
mysgantengs <iyangsahmad@gmail.com>
2024-01-31 20:54:43 +0700

    malam hari rabu ini kita commit lagi yak!

--- resources/views/Menu.blade.php
+++ resources/views/Menu.blade.php
@@ -1,31 +1,40 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1>Hello Menu</h1>
 
+<div class="row">
+    <h1 class="text-center">Hello Menu</h1><br>
+
+    <div class="col">
     <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+  <img src="bem.jpg" class="card-img-top" alt="...">
   <div class="card-body">
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
   </div>
 </div>
+</div>
 
 <br>
 
+<div class="col">
 <div class="card" style="width: 18rem;">
   <img src="..." class="card-img-top" alt="...">
   <div class="card-body">
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
   </div>
 </div>
+</div>
 
 <br>
 
+<div class="col">
 <div class="card" style="width: 18rem;">
   <img src="..." class="card-img-top" alt="...">
   <div class="card-body">
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
   </div>
+</div>
+</div>
 </div>
 @endsection    
 
