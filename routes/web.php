d03665c212255a5940764d967aec8a2187c9129f
mysgantengs <iyangsahmad@gmail.com>
2024-02-01 21:33:51 +0700

    kite ngomit lagi ye malam kamis ini partIV

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,27 @@ Route::get('/About', function () {
         'content' => ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque expedita repellendus, non eos reiciendis voluptatum labore totam consequatur, nemo eius hic corporis! Quod optio ad excepturi, voluptates perspiciatis ab beatae!Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae voluptatibus repellat illum hic, dolor enim magnam neque consectetur delectus veniam totam esse nihil impedit quos perferendis quibusdam sapiente, laboriosam iure?'
     ]);
 });
+
+// Route::get('/CrazyGirls', function () {
+//     return view('core_descriptions.CrazyGrils');    
+// });
+
+// Route::get('/CrazyGirls', function () {
+//     return view('core_descriptions.Endixy');    
+// });
+
+// Route::get('/CrazyGirls', function () {
+//     return view('core_descriptions.gambar1');    
+// });
+
+// Route::get('/CrazyGirls', function () {
+//     return view('core_descriptions.gambar2');    
+// });
+
+// Route::get('/CrazyGirls', function () {
+//     return view('core_descriptions.Spiderman');    
+// });
+
+// Route::get('/CrazyGirls', function () {
+//     return view('core_descriptions.Shadowshot');    
+// });
