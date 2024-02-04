9fb32e963404d17f606d3ad9ce56e6a814d14392
mysgantengs <iyangsahmad@gmail.com>
2024-02-05 01:40:25 +0700

    karena tidak bisa tidur maka kita ngoding ye heehehhhehehhe jilidII

--- resources/views/views.blade.php
+++ resources/views/views.blade.php
@@ -3,14 +3,14 @@
 @section('container')
 
 <div class="row">
-@foreach($Menus as $mess)
+
 <div class="col-md-4 mx-auto">       
-<h1 class="text-center">{{$mess['cardtitle']}}</h1><br>
-<img src="{{$mess['images']}}" alt="gambar" width="300x" height="300px">
+<h1 class="text-center">{{$Menus['cardtitle']}}</h1><br>
+<img src="{{$Menus['images']}}" alt="gambar" width="300x" height="300px">
 </div>
  </div>
- <p class="text-center">{{$mess['descriptions']}}</p>
-  @endforeach
+ <p class="text-center">{{$Menus['descriptions']}}</p>
+
 </div>
  @endsection
 
