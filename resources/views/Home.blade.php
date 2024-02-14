c61e0e64683669564e5553ce1e22cd8b00c2109b
mysgantengs <iyangsahmad@gmail.com>
2024-02-14 22:06:40 +0700

    commite

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -2,9 +2,11 @@
 @extends('layouts/main')
         
 @section('container')           
- <h1>Hello Reina</h1>
+ <h1 class="text-center">Hello Reina</h1>
 
- <img src="130515.jpg" alt="gambars" class="width-200px height=300px">
+ <img src="130515.jpg" class="img-fluid" alt="gambarku"><br><br>
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi quo error maiores quisquam? Eum, officia atque dolorum soluta dolorem nulla quod hic id, labore consequatur laboriosam vitae reiciendis ipsa non pariatur exercitationem accusantium incidunt! Beatae veritatis odit vero vitae minima, ducimus quaerat modi exercitationem, autem maiores ad, nostrum tempora est. Temporibus laudantium blanditiis, enim commodi maiores aperiam provident culpa unde distinctio, est deleniti nostrum molestiae eum. Obcaecati ipsa doloremque corrupti, incidunt aspernatur adipisci excepturi consequuntur illum placeat nam praesentium suscipit perferendis impedit necessitatibus dolore cupiditate tenetur iusto expedita eum ullam? Nesciunt voluptatem autem reiciendis commodi nobis sed id officia fuga! Voluptatem similique ullam repellendus quaerat laborum porro labore? Accusamus expedita qui ipsa eius autem consectetur minus officiis placeat fugiat quos sint quaerat quo, perferendis, architecto velit tempore vitae, voluptate in?
+</p>
 
 @endsection       
 
