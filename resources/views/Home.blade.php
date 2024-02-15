c4c40e2e0f7f97cbe403ab0f5994f886eb704aff
mysgantengs <iyangsahmad@gmail.com>
2024-02-15 10:29:49 +0700

    kamis menjelang siang ini kita ngomit larac lagi yak

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -2,11 +2,15 @@
 @extends('layouts/main')
         
 @section('container')           
- <h1 class="text-center">Hello Reina</h1>
 
- <img src="130515.jpg" class="img-fluid" alt="gambarku"><br><br>
+
+<img src="130515.jpg" class="img-fluid" alt="gambarku"><br><br>
+<h1 class="text-center right-60px">The Hounters The Japansese </h1><br>
 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi quo error maiores quisquam? Eum, officia atque dolorum soluta dolorem nulla quod hic id, labore consequatur laboriosam vitae reiciendis ipsa non pariatur exercitationem accusantium incidunt! Beatae veritatis odit vero vitae minima, ducimus quaerat modi exercitationem, autem maiores ad, nostrum tempora est. Temporibus laudantium blanditiis, enim commodi maiores aperiam provident culpa unde distinctio, est deleniti nostrum molestiae eum. Obcaecati ipsa doloremque corrupti, incidunt aspernatur adipisci excepturi consequuntur illum placeat nam praesentium suscipit perferendis impedit necessitatibus dolore cupiditate tenetur iusto expedita eum ullam? Nesciunt voluptatem autem reiciendis commodi nobis sed id officia fuga! Voluptatem similique ullam repellendus quaerat laborum porro labore? Accusamus expedita qui ipsa eius autem consectetur minus officiis placeat fugiat quos sint quaerat quo, perferendis, architecto velit tempore vitae, voluptate in?
-</p>
+</p><br>
+
+<img src="..." class="rounded float-start" alt="litle image">
+<img src="..." class="rounded float-end" alt="litle imageII">
 
 @endsection       
 
