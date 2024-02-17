72075c659aa1a84fcf2ddcc20e2790382945e265
mysgantengs <iyangsahmad@gmail.com>
2024-02-17 10:03:10 +0700

    sabtu pagi ini kita ngomit lagi part2

--- resources/views/About.blade.php
+++ resources/views/About.blade.php
@@ -4,7 +4,7 @@
     @section('container')
     
 <div class="card mb-3">
-  <img src="130515.jpg" class="card-img-top" alt="gambarku">
+  <img src="5126995.jpg" class="card-img-top" alt="gambarku">
   <div class="card-body">
     <h2 class="card-title text-center">Japanese Outourum's</h2>
     <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi quo error maiores quisquam? Eum, officia atque dolorum soluta dolorem nulla quod hic id, labore consequatur laboriosam vitae reiciendis ipsa non pariatur exercitationem accusantium incidunt! Beatae veritatis odit vero vitae minima, ducimus quaerat modi exercitationem, autem maiores ad, nostrum tempora est. Temporibus laudantium blanditiis, enim commodi maiores aperiam provident culpa unde distinctio, est deleniti nostrum molestiae eum. Obcaecati ipsa doloremque corrupti, incidunt aspernatur adipisci excepturi consequuntur illum placeat nam praesentium suscipit perferendis impedit necessitatibus dolore cupiditate tenetur iusto expedita eum ullam? Nesciunt voluptatem autem reiciendis commodi nobis sed id officia fuga! Voluptatem similique ullam repellendus quaerat laborum porro labore? Accusamus expedita qui ipsa eius autem consectetur minus officiis placeat fugiat quos sint quaerat quo, perferendis, architecto velit tempore vitae, voluptate in?</p>
--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -7,7 +7,7 @@
 
 <div class="card bg-dark">
    <div class="card-body">
-     <h1 class="text-white display-4">Hina'S Agawawa <br> The Sans</h1><br>
+     <h1 class="text-white display-4"><span class="text-primary">Hina'S Agasawa</span><br>The Sans</h1><br>
 <div class="row">
    <div class="col-md-5"> 
      <p class="text-white">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi quo error maiores quisquam? Eum, officia atque dolorum soluta dolorem nulla quod hic id, labore consequatur laboriosam vitae reiciendis ipsa non pariatur exercitationem accusantium incidunt! Beatae veritatis odit vero vitae minima, ducimus quaerat modi exercitationem, autem maiores ad, nostrum tempora est. Temporibus laudantium blanditiis, enim commodi maiores aperiam provident culpa unde distinctio, est deleniti nostrum molestiae eum. Obcaecati ipsa doloremque corrupti, incidunt aspernatur adipisci excepturi consequuntur illum placeat nam praesentium suscipit perferendis impedit necessitatibus dolore cupiditate tenetur iusto expedita eum ullam? Nesciunt voluptatem autem reiciendis commodi nobis sed id officia fuga! Voluptatem similique ullam repellendus quaerat laborum porro labore? Accusamus expedita qui ipsa eius autem consectetur minus officiis placeat fugiat quos sint quaerat quo, perferendis, architecto velit tempore vitae, voluptate in?</p><br>
