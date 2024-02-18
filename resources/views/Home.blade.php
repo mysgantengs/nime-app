02bfb1bf00711e20af46526f4d61793a44406903
mysgantengs <iyangsahmad@gmail.com>
2024-02-18 20:52:27 +0700

    kite ngomit lagi

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -13,7 +13,7 @@
    <p class="text-white">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi quo error maiores quisquam? Eum, officia atque dolorum soluta dolorem nulla quod hic id, labore consequatur laboriosam vitae reiciendis ipsa non pariatur exercitationem accusantium incidunt! Beatae veritatis odit vero vitae minima, ducimus quaerat modi exercitationem, autem maiores ad, nostrum tempora est. Temporibus laudantium blanditiis, enim commodi maiores aperiam provident culpa unde distinctio, est deleniti nostrum molestiae eum. Obcaecati ipsa doloremque corrupti, incidunt aspernatur adipisci excepturi consequuntur illum placeat nam praesentium suscipit perferendis impedit necessitatibus dolore cupiditate tenetur iusto expedita eum ullam? Nesciunt voluptatem autem reiciendis commodi nobis sed id officia fuga! Voluptatem similique ullam repellendus quaerat laborum porro labore? Accusamus expedita qui ipsa eius autem consectetur minus officiis placeat fugiat quos sint quaerat quo, perferendis, architecto velit tempore vitae, voluptate in?</p><br>
 
 
-   <img src="gambar-nimes.png" alt="nimes-cospl">
+   <img src="gambar-nimes.png" alt="nimes-cospl" class="bg-disp">
 
      <a href="" class="btn btn-danger">Sign-In</a>
      <a href="" class="btn btn-danger">Log-In</a>
