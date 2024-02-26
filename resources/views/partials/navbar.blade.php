472a59263150de5abcac649e286cdaed50fb552f
mysgantengs <iyangsahmad@gmail.com>
2024-02-26 13:29:08 +0700

    commite-commite

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-danger">
   <div class="container">
-    <a class="navbar-brand" href="#">MysNAV</a>
+    <a class="navbar-brand btn btn-warning" href="#">MysNAV</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
