74e3c2914d8c99a2ff6b22d877f97a0f726b9cba
mysgantengs <iyangsahmad@gmail.com>
2024-03-30 21:49:39 +0700

    ngomit lagi malam minggu ini ya!

--- routes/web.php
+++ routes/web.php
@@ -19,17 +19,17 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/Homes', [\App\Http\Controllers\ViewController::class, 'Home']);
-Route::get('/Menu', [\App\Http\Controllers\ViewController::class, 'Menu']);
+Route::get('/Menu', [\App\Http\Controllers\ViewController::class, 'Menu'])->middleware('auth');
 Route::get('/About', [\App\Http\Controllers\ViewController::class, 'About']);
 Route::get('/Menus', [\App\Http\Controllers\ViewController::class, 'ViewRender']);
-Route::get('/dashbord', [\App\Http\Controllers\DashboardController::class, 'viewDashboard']);
+Route::get('/dashbord', [\App\Http\Controllers\DashboardController::class, 'viewDashboard'])->middleware('auth');
 //Route::get('/Menus', [\App\Http\Controllers\ViewController::class, 'ViewRender']);
 Route::controller(\App\Http\Controllers\RegisterControlller::class)->group(function () {
-    Route::get('/register', 'ViewRegister');
+    Route::get('/register', 'ViewRegister')->middleware('guest');
     Route::post('/register', 'Registers');
 });
 Route::controller(\App\Http\Controllers\LoginControlller::class)->group(function () {
-    Route::get('/login', 'ViewLogin')->middleware('guest');
+    Route::get('/login', 'ViewLogin')->name('login')->middleware('guest');
     Route::post('/login', 'Logins');
-    Route::post('/logout', 'Logout');
+    Route::post('/logout', 'Logout')->middleware('auth');
 });
